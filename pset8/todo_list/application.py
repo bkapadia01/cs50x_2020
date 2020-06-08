3e6eb4fdf3108a8f5af4a19ef76184f11476fb30
Bhavin <bhavinkapadia89@gmail.com>
2020-06-07 23:39:40 -0400

    add sessions

--- pset8/todo_list/application.py
+++ pset8/todo_list/application.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = 'filesystem'
+Session(app)
 
 todos=[]
 
 @app.route("/")
 def tasks():
-    return  render_template("tasks.html", todos=todos)
+    if "todos" not in session:
+        session["todos"]=[]
+    return  render_template("tasks.html", todos=session["todos"])
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -14,5 +20,5 @@ def add():
         return render_template("add.html")
     else:
         todo = request.form.get("task")
-        todos.append(todo)
+        session["todos"].append(todo)
         return redirect("/")
